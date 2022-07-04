5e96de8c77fa852d971d92e07b220d3150cc2962
azumi-media <azumi.takahashi@gmo.media>
2022-07-04 17:33:47 +0900

    追いコミ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { RssListComponent } from "./rss-list/rss-list.component";
-import { RssListComponent as RssListAnswerComponent } from "./rss-list-answer/rss-list.component";
+import { RssListAnswerComponent } from "./rss-list-answer/rss-list.component";
 import { environment } from "src/environments/environment";
 
 @NgModule({
