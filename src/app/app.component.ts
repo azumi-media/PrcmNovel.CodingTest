be618506221af112a0afd2204a566453bb000f14
azumi-media <azumi.takahashi@gmo.media>
2022-07-04 17:41:17 +0900

    app component環境分け

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, VERSION } from '@angular/core';
 import { xml as sportsXml } from "@statics/sports";
 import { xml as itXml } from "@statics/it";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  templateUrl: environment.answer ? './app.component.answer.html' : './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
